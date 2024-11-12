3c1a97763e997075833346f233f80f4b494c3cb0
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-11-12 13:13:34 +0000

    Se ha hecho el ejercicio 1

--- app.js
+++ app.js
@@ -6,15 +6,37 @@
  * Función para buscar pokemon ejercicio 1 y 2.
  * 
 */
+let pokemonABuscar = document.getElementById("pokemon-input").value;
+let botonBusqueda = document.getElementById('search-btn');
+botonBusqueda.addEventListener('click', () => {
+    let nombrePokemon = pokemonABuscar.toLowerCase();
+    let url = "https://pokeapi.co/api/v2/pokemon/"+nombrePokemon;
+    console.log(buscarPokemon(url));
+});
 
-async function buscarPokemon(){
-    /**
-     * Tu código aquí.
-     */
+async function buscarPokemon(url){
+    try {
+        // Realiza la solicitud fetch y espera la respuesta
+        const respuesta = await fetch(url);
+        
+        // Verifica si la respuesta es exitosa
+        if (!respuesta.ok) {
+            throw new Error('Error en la red: ' + respuesta.status);
+        }
 
+        // Convierte la respuesta a JSON
+        const datos = await respuesta.json();
+
+        //Devolver el json
+        return datos;
+
+    } catch (error) {
+        console.error('Hubo un problema con la solicitud fetch:', error);
+    }
 
 
 }
+    
 
 /**
  *  Descomentar para hacer uso de la función.
@@ -40,3 +62,5 @@ $(document).ready(function(){
     $('#search-btn').on('click', buscarPokemonJQueryAJAX);
 }); 
 */
+
+
